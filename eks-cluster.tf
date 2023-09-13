2e68e5c9663dafc5b15f26d10cfae40c68b51016
Collins Orighose <collinsefe@yahoo.com>
2023-09-13 13:40:29 +0100

    cluster names and sg

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,15 +1,15 @@
-resource "aws_eks_cluster" "demo" {
+resource "aws_eks_cluster" "myapp_cluster" {
   name     = var.cluster-name
-  role_arn = aws_iam_role.demo-cluster.arn
+  role_arn = aws_iam_role.myapp_cluster.arn
 
   vpc_config {
-    security_group_ids = [aws_security_group.demo-cluster.id]
+    security_group_ids = [aws_security_group.myapp_cluster.id]
     subnet_ids = module.vpc.public_subnets
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.demo-cluster-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.demo-cluster-AmazonEKSServicePolicy,
+    aws_iam_role_policy_attachment.myapp_cluster_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.myapp_cluster_AmazonEKSServicePolicy
   ]
 }
 
