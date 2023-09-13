b36b692483e456986476718924d59c51dd49617f
Collins Orighose <collinsefe@yahoo.com>
2023-09-13 14:58:50 +0100

    add vars

--- providers.tf
+++ providers.tf
@@ -1,5 +1,8 @@
 provider "aws" {
-  region = "eu-west-2"
+  access_key = "${var.AWS_ACCESS_KEY}"
+  secret_key = "${var.AWS_SECRET_KEY}"
+  profile    = "${var.AWS_PROFILE}"
+  region     = "${var.AWS_REGION}"
 }
 
 data "aws_region" "current" {
