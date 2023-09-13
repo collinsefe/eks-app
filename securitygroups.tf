a2f4881ef69fd5ade1078f1afa0b0a534b8c2d9b
Collins Orighose <collinsefe@yahoo.com>
2023-09-13 13:40:51 +0100

    modified sg name

--- securitygroups.tf
+++ securitygroups.tf
@@ -1,5 +1,5 @@
-resource "aws_security_group" "demo-cluster" {
-  name        = "techbeat-eks-security-group"
+resource "aws_security_group" "myapp_cluster" {
+  name        = "myapp-eks-security-group"
   description = "Cluster communication with worker nodes"
   vpc_id      = module.vpc.vpc_id
 
@@ -11,26 +11,26 @@ resource "aws_security_group" "demo-cluster" {
   }
 
   tags = {
-    Name = "techbeat-eks-sg"
+    Name = "myapp-eks-sg"
   }
 }
 
-resource "aws_security_group_rule" "demo-cluster-ingress-node-https" {
+resource "aws_security_group_rule" "myapp-cluster-ingress-node-https" {
   description              = "Allow pods to communicate with the cluster API Server"
   from_port                = 443
   protocol                 = "tcp"
-  security_group_id        = aws_security_group.demo-cluster.id
-  source_security_group_id = aws_security_group.demo-node.id
+  security_group_id        = aws_security_group.myapp_cluster.id
+  source_security_group_id = aws_security_group.myapp_node.id
   to_port                  = 443
   type                     = "ingress"
 }
 
-resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
+resource "aws_security_group_rule" "myapp-cluster-ingress-workstation-https" {
   cidr_blocks       = [local.workstation-external-cidr]
   description       = "Allow workstation to communicate with the cluster API Server"
   from_port         = 443
   protocol          = "tcp"
-  security_group_id = aws_security_group.demo-cluster.id
+  security_group_id = aws_security_group.myapp_cluster.id
   to_port           = 443
   type              = "ingress"
 }
