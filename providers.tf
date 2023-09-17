11d9484e0afd751f35790a2876957c4185af73fb
Collins Orighose <collinsefe@yahoo.com>
2023-09-17 01:22:56 +0100

    code refactoring

--- providers.tf
+++ providers.tf
@@ -1,8 +1,4 @@
 provider "aws" {
-  access_key = "${var.AWS_ACCESS_KEY}"
-  secret_key = "${var.AWS_SECRET_KEY}"
-  profile    = "${var.AWS_PROFILE}"
-  region     = "${var.AWS_REGION}"
 }
 
 data "aws_region" "current" {
