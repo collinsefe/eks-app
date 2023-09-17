123adf7f71fbd439ccdbf707d64b45791e600603
Collins Orighose <collinsefe@yahoo.com>
2023-09-17 01:20:04 +0100

    code refactoring

--- vars.tf
+++ vars.tf
@@ -7,9 +7,7 @@ variable "AWS_ACCESS_KEY" {}
 
 variable "AWS_SECRET_KEY" {}
 
-variable "AWS_PROFILE" {
-  default = "bravo"
-}
+variable "AWS_PROFILE" {}
 
 variable "AWS_REGION" {
   default = "eu-west-2"
