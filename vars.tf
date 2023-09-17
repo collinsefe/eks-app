d2d3ee75fc1c89c94c2378e505ff3b14318beef8
Collins Orighose <collinsefe@yahoo.com>
2023-09-17 01:24:15 +0100

    code refactoring

--- vars.tf
+++ vars.tf
@@ -3,12 +3,6 @@ variable "cluster-name" {
   type    = string
 }
 
-variable "AWS_ACCESS_KEY" {}
-
-variable "AWS_SECRET_KEY" {}
-
-variable "AWS_PROFILE" {}
-
 variable "AWS_REGION" {
   default = "eu-west-2"
 }
